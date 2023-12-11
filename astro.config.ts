9a3d632594f2828628678508c50b5969d6d0761f
Josh Chang <joshchang04@gmail.com>
2023-12-11 06:00:13 -0500

    Update Astro config and add redirect in index page

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
+	output: "server",
 	integrations: [tailwind(), prefetch()],
 	adapter: cloudflare(),
 });
