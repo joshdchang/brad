0cc5c64165cde480f75bccdc6fbb53931b6bebd4
Josh Chang <joshchang04@gmail.com>
2023-12-11 06:07:37 -0500

    Remove prefetch integration from astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), prefetch()],
+  integrations: [tailwind()],
   adapter: vercel()
 });
