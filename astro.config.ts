4abad31513af67c363f7690bee5b692dc4841481
Josh Chang <joshchang04@gmail.com>
2023-12-11 06:06:41 -0500

    Update dependencies and adapter in package.json and astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [tailwind(), prefetch()],
-	adapter: cloudflare(),
+  output: "server",
+  integrations: [tailwind(), prefetch()],
+  adapter: vercel()
 });
