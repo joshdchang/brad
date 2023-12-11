02fe10715b012164bbbf95078cd660a4ea4bb62a
Josh Chang <joshchang04@gmail.com>
2023-12-11 07:29:31 -0500

    Remove unused frame options and update dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   adapter: vercel({
-		edgeMiddleware: true,
-	})
+    edgeMiddleware: true
+  })
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,9 @@
-import { z, defineCollection, reference } from "astro:content";
+import { z, defineCollection } from "astro:content";
 
 const frame = defineCollection({
   type: "content", 
   schema: () => z.object({
     title: z.string(),
-    options: z.array(z.object({
-      label: z.string(),
-      link: reference("frame").optional(),
-    })).optional(),
   }),
 });
 
