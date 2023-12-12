6dd49ea9e420ae263c75f0686b00a923872e055c
Josh Chang <joshchang04@gmail.com>
2023-12-12 00:26:14 -0500

    Remove unused code and update dependencies

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-// export function pathToSlug(path: string | undefined) {
-// 	return path?.split("src/content/frame/")[1]?.split(".md")[0];
-// }
-
-import { createReader } from "@keystatic/core/reader";
-import keystaticConfig from "../../keystatic.config";
-
-export const reader = createReader(process.cwd(), keystaticConfig);
